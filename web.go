237d19f8cefe689c7d5bdcb2465b20b8bc46ed80
stujo <stujo@imagescript.com>
2014-05-22 22:02:28 -0700

    Adding Template

--- web.go
+++ web.go
@@ -5,18 +5,50 @@ import (
 	"net/http"
 	"os"
 	"runtime"
+	"html/template"
+	"time"
 )
 
+var templates = template.Must(template.ParseFiles("index.html"))
+
+type Greeting struct {
+	Version string
+	Time    string
+}
+
 func main() {
-	http.HandleFunc("/", hello)
-	bind := fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT"))
+
+	http.HandleFunc("/", sayhello)
+
+	var hostname = "localhost"
+	var port = "3337"
+
+	if os.Getenv("HOST") == "" {
+		hostname = os.Getenv("HOST")
+	}
+
+	if os.Getenv("PORT") == "" {
+		port = os.Getenv("PORT")
+	}
+
+	//Using Default localhost
+	bind := fmt.Sprintf("%s:%s", hostname, port)
+
 	fmt.Printf("listening on %s...", bind)
+
 	err := http.ListenAndServe(bind, nil)
+
 	if err != nil {
 		panic(err)
 	}
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "Stujo's Go app says 'Hello, world from runtime.Version(%s)'", runtime.Version())
+func sayhello(res http.ResponseWriter, req *http.Request) {
+
+	greeting := &Greeting{Version: runtime.Version(), Time: time.Now().String()}
+
+	err := templates.ExecuteTemplate(res, "index.html", greeting)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	}
 }
